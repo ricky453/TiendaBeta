90be1021a0e768dad30d51ca869288809df0ef5c
Ricky <Ricky@LAPTOP-RICKY>
2017-06-19 17:08:11 -0600

    Notificaciones

--- src/formularios/frmNotificacion.java
+++ src/formularios/frmNotificacion.java
@@ -43,12 +43,12 @@ public class frmNotificacion extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setAutoRequestFocus(false);
-        setMaximumSize(new java.awt.Dimension(378, 59));
-        setMinimumSize(new java.awt.Dimension(378, 59));
+        setMaximumSize(new java.awt.Dimension(409, 59));
+        setMinimumSize(new java.awt.Dimension(409, 59));
         setModalExclusionType(java.awt.Dialog.ModalExclusionType.APPLICATION_EXCLUDE);
         setUndecorated(true);
         setOpacity(0.95F);
-        setPreferredSize(new java.awt.Dimension(378, 60));
+        setPreferredSize(new java.awt.Dimension(409, 60));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel1.setBackground(new java.awt.Color(4, 3, 1));
@@ -57,12 +57,12 @@ public class frmNotificacion extends javax.swing.JFrame {
         lblMensaje.setFont(new java.awt.Font("DejaVu Sans", 1, 15)); // NOI18N
         lblMensaje.setForeground(new java.awt.Color(254, 254, 254));
         lblMensaje.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jPanel1.add(lblMensaje, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 10, 280, 40));
+        jPanel1.add(lblMensaje, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 10, 320, 40));
 
         lblIcono.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jPanel1.add(lblIcono, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 50, 40));
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 380, 60));
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 410, 60));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
