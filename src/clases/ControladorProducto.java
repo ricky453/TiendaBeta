8e6f4596c604db667a9b5dd602ea2242e1eacf1f
José López <Jos Lpez@192.168.0.17>
2017-06-18 22:22:50 -0600

    VENTAS... ObetenerProducto y ObtenerSucursales, diferente de sergioo xD

--- src/clases/ControladorProducto.java
+++ src/clases/ControladorProducto.java
@@ -120,6 +120,7 @@ public class ControladorProducto {
         try {
             rs=cn.st.executeQuery("SELECT Inventario.IdSucursal, Inventario.Cantidad, Inventario.CodBarra,Producto.Nombre,Producto.Costo FROM Inventario,Producto WHERE  Inventario.CodBarra='"+CodBarra+"' AND Inventario.IdSucursal="+idSucursal+" AND Producto.CodBarra=Inventario.CodBarra;");
             while (rs.next()) {
+                
                 miproducto.setCodBarra(rs.getString(3));
                 miproducto.setNombre(rs.getString(4));
                 miproducto.setInventario(Integer.parseInt(rs.getString(2)));
